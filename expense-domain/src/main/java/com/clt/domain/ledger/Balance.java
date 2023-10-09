eb243510544197940a1c8d9cbb4c4abde424f5cf
cirius1792 <cirolucio.tecce@gmail.com>
2023-10-09 20:44:53 +0200

    [NEW] Personal Balance to track amount owned

--- expense-domain/src/main/java/com/clt/domain/ledger/Balance.java
+++ expense-domain/src/main/java/com/clt/domain/ledger/Balance.java
@@ -3,17 +3,56 @@ package com.clt.domain.ledger;
 import com.clt.domain.expense.Money;
 import com.clt.domain.group.Person;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface Balance {
-  Map<Person, Money> owned(Person debtor);
+public class Balance {
+    private final Person owner;
+    private List<ExpenseCharge> charges;
 
-  default Money totalOwned(Person debtor) {
-    return Optional.ofNullable(owned(debtor))
-            .orElse(new HashMap<>())
-            .values().stream()
-            .reduce(Money.euros(0), Money::plus);
-  }
+    public Balance(Person owner) {
+        this.owner = owner;
+        charges = new ArrayList<>();
+    }
+
+    public Money balance(){
+        return this.credits().minus(this.debits());
+    }
+    private Money debits() {
+        return this.charges.stream()
+                .filter(c -> this.owner.id().equals(c.debtor().id()))
+                .map(ExpenseCharge::dueAmount)
+                .reduce(Money.euros(0), Money::plus);
+    }
+
+    private Money credits() {
+        return this.charges.stream()
+                .filter(c -> this.owner.id().equals(c.creditor().id()))
+                .map(ExpenseCharge::dueAmount)
+                .reduce(Money.euros(0), Money::plus);
+
+    }
+
+    public void addExpenseCharge(ExpenseCharge charge) {
+        if (!this.owner.id().equals(charge.creditor().id())
+                && !this.owner.id().equals(charge.debtor().id()))
+            throw new IllegalArgumentException("The balance owner is not the debtor nor the creditor");
+        this.charges.add(charge);
+    }
+
+    public List<ExpenseCharge> getExpenseCharges() {
+        return new ArrayList<>(this.charges);
+    }
+
+    public Money getDueTo(Person person) {
+        Money debits = this.charges.stream()
+                .filter(c -> person.id().equals(c.creditor().id()))
+                .map(ExpenseCharge::dueAmount)
+                .reduce(Money.euros(0), Money::plus);
+        Money credits = this.charges.stream()
+                .filter(c -> person.id().equals(c.debtor().id()))
+                .map(ExpenseCharge::dueAmount)
+                .reduce(Money.euros(0), Money::plus);
+        return credits.minus(debits).negate();
+    }
 }
--- expense-domain/src/test/java/com/clt/domain/ledger/BalanceEvaluatorTest.java
+++ expense-domain/src/test/java/com/clt/domain/ledger/BalanceEvaluatorTest.java
@@ -7,87 +7,106 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 class BalanceEvaluatorTest {
 
     private static final Person ALICE = PersonUtil.newPerson();
     private static final Person BOB = PersonUtil.newPerson();
     private static final Person SARA = PersonUtil.newPerson();
-    private static final ExpenseCharge EC11 =
+    private static final ExpenseCharge EXPENSE_CHARGE_FROM_ALICE_TO_BOB =
             ImmutableExpenseCharge.builder()
-                    .id("ec11")
+                    .id("EXPENSE_CHARGE_FROM_ALICE_TO_BOB")
                     .debtor(BOB)
                     .creditor(ALICE)
                     .expense("e1")
                     .groupId("g1")
-                    .dueAmount(Money.euros(1))
+                    .dueAmount(Money.euros(2))
                     .build();
-    private static final ExpenseCharge EC12 =
+    private static final ExpenseCharge EXPENSE_CHARGE_FROM_BOB_TO_ALICE =
             ImmutableExpenseCharge.builder()
-                    .id("ec12")
-                    .debtor(SARA)
-                    .creditor(ALICE)
-                    .expense("e1")
+                    .id("EXPENSE_CHARGE_FROM_BOB_TO_ALICE")
+                    .creditor(BOB)
+                    .debtor(ALICE)
+                    .expense("e2")
                     .groupId("g1")
                     .dueAmount(Money.euros(1))
                     .build();
 
-   private static final ExpenseCharge EC21 =
+    private static final ExpenseCharge EXPENSE_CHARGE_FROM_BOB_TO_ALICE_2 =
             ImmutableExpenseCharge.builder()
-                    .id("ec21")
-                    .debtor(ALICE)
+                    .id("EXPENSE_CHARGE_FROM_BOB_TO_ALICE_2")
                     .creditor(BOB)
-                    .expense("e2")
-                    .groupId("g1")
-                    .dueAmount(Money.euros(2.94))
-                    .build();
-    private static final ExpenseCharge EC22 =
-            ImmutableExpenseCharge.builder()
-                    .id("ec22")
-                    .debtor(SARA)
-                    .creditor(BOB)
-                    .expense("e2")
-                    .groupId("g1")
-                    .dueAmount(Money.euros(2.94))
-                    .build();
-
-   private static final ExpenseCharge EC31 =
-            ImmutableExpenseCharge.builder()
-                    .id("ec31")
                     .debtor(ALICE)
-                    .creditor(SARA)
                     .expense("e3")
                     .groupId("g1")
-                    .dueAmount(Money.euros(1.15))
+                    .dueAmount(Money.euros(6))
                     .build();
-    private static final ExpenseCharge EC32 =
+    private static final ExpenseCharge EXPENSE_CHARGE_FROM_SARA_TO_BOB =
             ImmutableExpenseCharge.builder()
-                    .id("ec32")
+                    .id("EXPENSE_CHARGE_FROM_SARA_TO_BOB")
+                    .creditor(SARA)
                     .debtor(BOB)
-                    .creditor(SARA)
-                    .expense("e3")
+                    .expense("e4")
                     .groupId("g1")
-                    .dueAmount(Money.euros(1.15))
+                    .dueAmount(Money.euros(3))
                     .build();
-    private static final List<ExpenseCharge> CHARGES =
-            Arrays.asList(
-                    EC11, EC12,
-                    EC21, EC22,
-                    EC31, EC32);
 
-    BalanceEvaluator balanceEvaluator = new BalanceEvaluator();
 
-    @DisplayName(
-            "Given a group of 3 people"
-                    + "And a list of expenses made by those people "
-                    + "When evaluating the balance "
-                    + "Then each person has a balance")
+    @DisplayName("Given the balance of BOB " +
+            "And an Expense charge of 2€ from ALICE " +
+            "When the expense is added to the balance " +
+            "Then the debits are of 2€")
     @Test
-    void balance_evaluation_test() {
-        Balance actual = balanceEvaluator.evaluate(CHARGES);
-        Money ownedByBob = EC32.dueAmount().plus(EC11.dueAmount());
-        Assertions.assertEquals(ownedByBob, actual.totalOwned(BOB));
+    void add_one_expense_test() {
+        Balance balance = new Balance(BOB);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_ALICE_TO_BOB);
+        Assertions.assertEquals(EXPENSE_CHARGE_FROM_ALICE_TO_BOB.dueAmount().negate(), balance.balance());
     }
+
+    @DisplayName("Given the balance of BOB " +
+            "And an Expense charge of 2€ from ALICE " +
+            "And an Expense charge of 1€ from BOB " +
+            "When the expense is added to the balance " +
+            "Then the debits are of 1€ ")
+    @Test
+    void add_two_expense_test() {
+        Balance balance = new Balance(BOB);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_ALICE_TO_BOB);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_BOB_TO_ALICE);
+
+        Assertions.assertEquals(Money.euros(-1), balance.balance());
+    }
+
+    @DisplayName("Given the balance of BOB " +
+            "And an Expense charge of 2€ from ALICE " +
+            "And an Expense charge of 6€ from BOB " +
+            "And an expense charge of 3€ from SARA " +
+            "When the expense is added to the balance " +
+            "Then the credits are of 1€")
+    @Test
+    void add_three_expenses_test(){
+        Balance balance = new Balance(BOB);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_ALICE_TO_BOB);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_BOB_TO_ALICE_2);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_SARA_TO_BOB);
+
+        Assertions.assertEquals(Money.euros(1), balance.balance());
+    }
+    @DisplayName("Given the balance of BOB " +
+            "And an Expense charge of 2€ from ALICE " +
+            "And an Expense charge of 6€ from BOB to ALICE " +
+            "And an expense charge of 3€ from SARA " +
+            "When the expense is added to the balance " +
+            "Then 4€ are due from ALICE to BOB " +
+            "And 3€ are due from BOB to SARA")
+    @Test
+    void test_balance_per_person(){
+        Balance balance = new Balance(BOB);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_ALICE_TO_BOB);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_BOB_TO_ALICE_2);
+        balance.addExpenseCharge(EXPENSE_CHARGE_FROM_SARA_TO_BOB);
+
+        Assertions.assertEquals(Money.euros(-4), balance.getDueTo(ALICE));
+        Assertions.assertEquals(Money.euros(3), balance.getDueTo(SARA));
+    }
+
 }
