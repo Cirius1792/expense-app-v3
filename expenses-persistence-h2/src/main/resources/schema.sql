ae0bdca315e1c675dbcc8a2ab24e32aba6f8d035
cirius1792 <cirolucio.tecce@gmail.com>
2023-10-20 10:31:53 +0200

    Added schema definition for group an person tables

--- expenses-persistence-h2/src/main/resources/schema.sql
+++ expenses-persistence-h2/src/main/resources/schema.sql
@@ -4,8 +4,19 @@ CREATE TABLE expense ( id VARCHAR PRIMARY KEY,
                         owner_id VARCHAR(100) NOT NULL,
                         group_id VARCHAR(100) NOT NULL
                         );
-
 CREATE TABLE person (
                         id VARCHAR PRIMARY KEY,
                         username VARCHAR(255)
                         );
+CREATE TABLE expense_group (
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR(255),
+    OWNER VARCHAR(100) NOT NULL
+    );
+CREATE TABLE group_member (
+    group_id VARCHAR,
+    member VARCHAR(100),
+    PRIMARY KEY (group_id, member)
+    );
+
+
