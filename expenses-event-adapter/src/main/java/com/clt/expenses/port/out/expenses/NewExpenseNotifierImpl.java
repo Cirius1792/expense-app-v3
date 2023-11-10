7cf5124ccd0ab51312e716e18d79a34e5c678d84
cirius1792 <cirolucio.tecce@gmail.com>
2023-11-10 18:37:22 +0100

    Added Missing class ExpenseNotifier

---
+++ expenses-event-adapter/src/main/java/com/clt/expenses/port/out/expenses/NewExpenseNotifierImpl.java
@@ -0,0 +1,42 @@
+package com.clt.expenses.port.out.expenses;
+
+import com.clt.domain.expense.ExpenseRecord;
+import com.clt.event.GenericEvent;
+import com.clt.event.Notifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.UUID;
+
+@Service
+public class NewExpenseNotifierImpl implements Notifier<ExpenseRecord> {
+  private static final Logger logger = LoggerFactory.getLogger(NewExpenseNotifierImpl.class);
+  private final RabbitTemplate template;
+
+  private final Queue newExpenseQueue;
+
+  public NewExpenseNotifierImpl(RabbitTemplate template, Queue newExpenseQueue) {
+    logger.info("Initializing Expense Notifier");
+    this.template = template;
+    this.newExpenseQueue = newExpenseQueue;
+  }
+
+  @Override
+  public Mono<GenericEvent<ExpenseRecord>> notify(ExpenseRecord expenseRecord) {
+    return Mono.fromCallable(() -> sendEvent(expenseRecord))
+        .subscribeOn(Schedulers.boundedElastic());
+  }
+
+  private GenericEvent<ExpenseRecord> sendEvent(ExpenseRecord expenseRecord) {
+    logger.info("Sending: {}", expenseRecord);
+    GenericEvent<ExpenseRecord> event =
+        new GenericEvent<>(UUID.randomUUID().toString(), expenseRecord);
+    this.template.convertAndSend(this.newExpenseQueue.getName(), event);
+    return event;
+  }
+}
