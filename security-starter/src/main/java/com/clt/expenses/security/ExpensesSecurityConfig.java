ccfec6c52fb419ae642cf9f3c9a6444f06af242b
cirius1792 <cirolucio.tecce@gmail.com>
2024-01-12 23:43:34 +0100

    chore: code formatted

--- security-starter/src/main/java/com/clt/expenses/security/ExpensesSecurityConfig.java
+++ security-starter/src/main/java/com/clt/expenses/security/ExpensesSecurityConfig.java
@@ -27,7 +27,7 @@ public class ExpensesSecurityConfig {
 
     @Bean
     @Profile({"test"})
-    public MapReactiveUserDetailsService userDetailsManager(PasswordEncoder passwordEncoder){
+    public MapReactiveUserDetailsService userDetailsManager(PasswordEncoder passwordEncoder) {
         UserDetails user =
                 User.builder()
                         .username("admin")
@@ -36,16 +36,22 @@ public class ExpensesSecurityConfig {
                         .build();
         return new MapReactiveUserDetailsService(user);
     }
+
     @Bean
-    public ReactiveUserDetailsService reactiveUserDetailsService(ApplicationCredentialManager applicationCredentialManager){
+    public ReactiveUserDetailsService reactiveUserDetailsService(
+            ApplicationCredentialManager applicationCredentialManager) {
         return new UserDetailService(applicationCredentialManager);
     }
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
-        http.authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/user").permitAll()
-                        .anyExchange().authenticated())
+        http.authorizeExchange(
+                        exchanges ->
+                                exchanges
+                                        .pathMatchers("/user")
+                                        .permitAll()
+                                        .anyExchange()
+                                        .authenticated())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .httpBasic(Customizer.withDefaults());
         return http.build();
