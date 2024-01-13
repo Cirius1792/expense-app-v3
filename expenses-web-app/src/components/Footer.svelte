d581a1c85f26c69a19f983a6fa49fe346206677f
Ciro Lucio Tecce <cirolucio.tecce@gmail.com>
2024-01-13 18:15:47 +0100

    [NEW] Web App Scaffolding
    Created the skeleton for the web app using Svelte

---
+++ expenses-web-app/src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<footer class="footer">
+  <h3 class="h3">Just Share</h3>
+</footer>
+
+<style>
+  .footer {
+    position: relative;
+    margin-bottom: 2rem;
+    border-radius: 5px;
+    padding: 1rem;
+    overflow: hidden;
+    background-color: #4caf50; /* Change this value to your preferred green color */
+    text-align: center;
+  }
+
+  .h3 {
+    padding: 0.1 1rem;
+    color: #fff;
+    font-size: 1.0;
+    font-weight: bold;
+  }
+</style>
